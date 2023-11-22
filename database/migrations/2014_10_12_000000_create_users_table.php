d7afe1b8f73f5428ca83295ab22a1b000aa2b2bd
terenyijanos96 <terenyi.janos96@gmail.com>
2023-11-22 17:39:52 +0100

    Migrációs fájlok kitöltése

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->integer('balance');
             $table->rememberToken();
             $table->timestamps();
         });
